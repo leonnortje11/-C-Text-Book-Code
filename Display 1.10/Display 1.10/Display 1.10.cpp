b85113222f9efe36ebe94f80633f10f088f9c668
Leon Nortje <leon.nortje@outlook.com>
2015-07-26 09:14:09 +0200

    OK

---
+++ Display 1.10/Display 1.10/Display 1.10.cpp
@@ -0,0 +1,15 @@
+// Display 1.10.cpp : Defines the entry point for the console application.
+// Testing Your C++ Setup
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Testing 1, 2, 3\n";
+
+	return 0;
+}
+
