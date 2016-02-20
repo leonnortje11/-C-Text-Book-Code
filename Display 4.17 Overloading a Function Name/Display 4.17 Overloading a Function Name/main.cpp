047c4f75234f21c1fe9d21cdeeaed729d639c397
leonnortje11 <nortje.leon@gmail.com>
2016-02-20 18:50:42 +0200

    OK

--- Display 4.17 Overloading a Function Name/Display 4.17 Overloading a Function Name/main.cpp
+++ Display 4.17 Overloading a Function Name/Display 4.17 Overloading a Function Name/main.cpp
@@ -4,11 +4,19 @@
 //
 //  Created by Leon Nortje on 2016/02/20.
 //  Copyright © 2016 Leon Nortje. All rights reserved.
-//
+//  //Illustrates overloading the function name ave.
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+double ave(double n1, double n2);
+//Returns the average of the two numbers n1 and n2.
+
+double ave(double n1, double n2, double n3);
+//Returns the average of the three numbers n1, and n3.
+
+int main()
+{
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
