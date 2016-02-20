1bfed87fc51628859f3b5f119b9889401b85fdad
leonnortje11 <nortje.leon@gmail.com>
2016-02-20 18:43:10 +0200

    OK

---
+++ Display 4.17 Overloading a Function Name/Display 4.17 Overloading a Function Name/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Display 4.17 Overloading a Function Name
+//
+//  Created by Leon Nortje on 2016/02/20.
+//  Copyright © 2016 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
