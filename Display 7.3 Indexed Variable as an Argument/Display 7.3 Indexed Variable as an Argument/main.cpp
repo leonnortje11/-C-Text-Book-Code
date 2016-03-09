802c31d9e9a25d48fff630a833169cced289d670
leonnortje11 <nortje.leon@gmail.com>
2016-03-09 06:11:40 +0200

    Done

---
+++ Display 7.3 Indexed Variable as an Argument/Display 7.3 Indexed Variable as an Argument/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  Display 7.3 Indexed Variable as an Argument
+//
+//  Created by Leon Nortje on 2016/03/09.
+//  Copyright © 2016 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+
+
+const int NUMBER_OF_EMPLOYEES = 3;
+
+int adjust_days(int old_days);
+//return old_days plus 5.
+
+int main()
+{
+    using namespace std;
+    int vacation[NUMBER_OF_EMPLOYEES], number;
+    
+    cout << "Enter allowed vacation days for employees 1"
+        << " through " << NUMBER_OF_EMPLOYEES << ":\n";
+    
+    for (number = 1; number <= NUMBER_OF_EMPLOYEES; number++)
+        cin >> vacation[number-1];
+    
+    for (number = 0; number < NUMBER_OF_EMPLOYEES; number++)
+        vacation[number] = adjust_days(vacation[number]);
+    
+    cout << "The revised number of vacation days are:\n";
+    for (number = 1; number <= NUMBER_OF_EMPLOYEES; number++)
+        
+    cout << "Employee number " << number
+    << " vacation days = " << vacation[number-1] << endl;
+    
+    return 0;
+}
+
+int adjust_days(int old_days)
+{
+    return (old_days + 5);
+}
