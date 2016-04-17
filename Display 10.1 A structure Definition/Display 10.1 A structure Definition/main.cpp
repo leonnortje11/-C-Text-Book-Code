56b68bb45221767a4e73c46f0e764bcc7f936af3
leonnortje11 <nortje.leon@gmail.com>
2016-04-17 12:16:33 +0200

    Final

--- Display 10.1 A structure Definition/Display 10.1 A structure Definition/main.cpp
+++ Display 10.1 A structure Definition/Display 10.1 A structure Definition/main.cpp
@@ -17,9 +17,41 @@ struct CDAccount
     int term; //months until maturity
 };
 
+void get_data(CDAccount& the_account);
+//Postconditions: the_account.balance and the_account.interest_rate have been given values that the user entered at the keyboard
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    CDAccount account;
+    get_data(account);
+    
+    double rate_fraction, interest;
+    rate_fraction = account.interest_rate/100.0;
+    interest = account.balance*rate_fraction*(account.term/12.0);
+    account.balance = account.balance + interest;
+    
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    
+    cout << "When your CD matures in "
+        << account.term << " months,\n"
+        << "it will have a balance of R"
+    << account.balance << endl;
+
     return 0;
+}
+
+//Uses iostream:
+void get_data(CDAccount& the_account)
+{
+    cout << "Enter account balance: R";
+    cin >> the_account.balance;
+    
+    cout << "Enter account interest rate: ";
+    cin >> the_account.interest_rate;
+    
+    cout << "Enter the number of months until maturity\n"
+        << "(must be 12 or fewer months): ";
+    cin >> the_account.term;
 }
