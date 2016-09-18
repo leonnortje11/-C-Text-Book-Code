e9938d206530cf824aa720fb56a19c33b324072f
leonnortje11 <nortje.leon@gmail.com>
2016-09-18 09:23:21 +0200

    OK

---
+++ Display 10.6 Class with Constructors/Display 10.6 Class with Constructors/main.cpp
@@ -0,0 +1,18 @@
+//  Program to demonstrate the class BankAccount
+//  main.cpp
+//  Display 10.6 Class with Constructors
+//
+//  Created by Leon Nortje on 2016/09/18.
+//  Copyright © 2016 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+//  Class for a bank account
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
