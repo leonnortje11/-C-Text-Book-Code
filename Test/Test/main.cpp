dd53eda99adfddfd141e249211f5aedf151aef21
leonnortje11 <nortje.leon@gmail.com>
2017-02-05 22:20:36 +0200

    OK

---
+++ Test/Test/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Leon Nortje on 2017/02/05.
+//  Copyright © 2017 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
