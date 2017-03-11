fb4fc919005098830b4733b5d7a68c7fabcd8c1c
leonnortje11 <leon.nortje@outlook.com>
2017-03-11 11:50:51 +0200

    Test

---
+++ Test from Mac/Test from Mac/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Test from Mac
+//
+//  Created by Leon Nortje on 2017/03/11.
+//  Copyright © 2017 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    cout << "Hello, World!\n";
+    
+    system("pause");
+    
+    return 0;
+}
